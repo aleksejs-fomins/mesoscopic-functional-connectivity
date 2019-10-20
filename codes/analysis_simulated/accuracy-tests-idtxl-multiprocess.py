ce6b381aa78ab539af8ca29187c60d1b403214c1
Aleksejs Fomins <aleksejs.fomins@uzh.ch>
2019-10-20 22:09:16 +0200

    analysis-sweep tidy

--- codes/analysis_simulated/accuracy-tests-idtxl-multiprocess.py
+++ codes/analysis_simulated/accuracy-tests-idtxl-multiprocess.py
@@ -1,4 +1,10 @@
+import os, sys
+import h5py
+import numpy as np
+
 from codes.lib.data_io.qt_wrapper import gui_fnames
+from codes.lib.fc.te_idtxl_wrapper import idtxlParallelCPUMulti, idtxlResultsParse
+from codes.lib.plots.accuracy_plots import testplots
 
 '''
   Width/Depth Test Code:
@@ -19,54 +25,60 @@ from codes.lib.data_io.qt_wrapper import gui_fnames
     Make Plots, Store TE
 '''
 
+nNode = 12
 dataFileNames = gui_fnames("Get simulated data files", "./", "hdf5 (*.h5)")
 
 #############################
 # IDTxl parameters
 #############################
 idtxl_settings = {
-    'dim_order'       : 'ps',
-    'method'          : 'MultivariateTE',
+    'dim_order'       : 'rsp',
+    'methods'         : ['BivariateMI', 'MultivariateMI', 'BivariateTE', 'MultivariateTE'],
     'cmi_estimator'   : 'JidtGaussianCMI',
     'max_lag_sources' : 1,
     'min_lag_sources' : 1}
 
-idtxl_methods = ['BivariateMI', 'MultivariateMI', 'BivariateTE', 'MultivariateTE']
-
 #############################
-# Depth Tests
+# Width / Depth Tests
 #############################
 
-print("Performing Depth Tests")
-depthFileNames = [fname for fname in dataFileNames if "depth" in fname]
+for analysis in ["width", "depth"]:
+    analysisFileNames = [fname for fname in dataFileNames if analysis in fname]
+    print("Performing",analysis,"tests on", len(analysisFileNames), "files")
 
-for fname in depthFileNames:
-    # Read file here
+    for modelName in ["purenoise", "lpfsubnoise", "dynsys"]:
+        modelFileNames = np.sort([fname for fname in analysisFileNames if modelName in fname])
+        print("- For model", modelName, len(modelFileNames), "files")
 
-    for method in idtxl_methods:
-        idtxl_settings['method'] = method
-        te_results = np.zeros((3, N_NODE, N_NODE, N_STEP))
+        nFile = len(modelFileNames)
+        te_results = { key : np.zeros((3, nNode, nNode, nFile)) for key in idtxl_methods }
 
-        for i, ndata in enumerate(ndata_lst):
-            print("Processing Data", analysis, method, modelname, ndata)
+        nDataEff = []
+        dataLst = []
+        for iFile, fname in enumerate(modelFileNames):
+            print("-- Reading Data File", os.path.basename(fname))
 
-            # Run calculation
-            rez = idtxlParallelCPU(data_lst[i], idtxl_settings)
+            # Read file here
+            with h5py.File(fname, "r") as h5f:
+                modelName = str(h5f['results']['modelName'])
+                trueConn = np.copy(h5f['results']['connTrue'])
+                dataLst += [np.copy(h5f['results']['data'])]
+                nTrial, nTime, nNode = dataLst[-1].shape
+                nDataEff += [nTrial * nTime]
+
+
+        # Run calculation
+        rez = idtxlParallelCPUMulti(dataLst, idtxl_settings, analysis+"_"+modelName)
+
+        for iMethod, method in enumerate(idtxl_settings['methods']):
+            fname = analysis + "_" + modelName + '_' + str(nNode) + '_' + method
+            te_data = np.full((3, nNode, nNode, nFile), np.nan)
 
             # Parse Data
-            te_results[..., i] = np.array(idtxlResultsParse(rez, N_NODE, method=method, storage='matrix'))
+            for iFile in range(nFile):
+                te_data[..., iFile] = np.array(idtxlResultsParse(rez[iMethod][iFile], nNode, method=method, storage='matrix'))
 
-        # Plot
-        fname = modelname + '_' + str(N_NODE) + method + '_' + analysis
-        testplots(ndata_eff, te_results, TRUE_CONN, logx=True, percenty=True, pTHR=0.01, h5_fname=fname + '.h5',
-                  fig_fname=fname + '.png')
-
-
-#############################
-# Width Tests
-#############################
-
-
+            testplots(nDataEff, te_data, trueConn, logx=True, percenty=True, pTHR=0.01, h5_fname=fname + '.h5', fig_fname=fname + '.png')
 
 #############################
 # SNR Tests
